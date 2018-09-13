c3421b166314249e2111c312b49b93bfa34ac773
Thomas Weichselbaumer <kontakt@themecoder.de>
2018-09-13 12:10:09 +0200

    added new editor-style.css for Gutenberg 3.8

--- inc/gutenberg.php
+++ inc/gutenberg.php
@@ -96,6 +96,10 @@ function gt_health_gutenberg_support() {
 			'slug'      => 'extra-large',
 		),
 	) );
+
+	// Add Editor Styles.
+	add_editor_style( 'assets/css/editor-style.css' );
+	add_theme_support( 'editor-styles' );
 }
 add_action( 'after_setup_theme', 'gt_health_gutenberg_support' );
 
