ba7f18ab4c43611d32d56cbb57376494e817b239
Thomas Weichselbaumer <kontakt@themecoder.de>
2019-12-12 10:50:40 +0100

    load custom fonts from GT Local Fonts plugin in Customizer preview

--- assets/js/customize-preview.js
+++ assets/js/customize-preview.js
@@ -270,16 +270,8 @@
 	wp.customize( 'gt_focus_theme_options[text_font]', function( value ) {
 		value.bind( function( newval ) {
 
-			// Embed Font.
-			var fontFamilyUrl = newval.split( " " ).join( "+" );
-			var googleFontPath = "https://fonts.googleapis.com/css?family=" + fontFamilyUrl + ":400,700";
-			var googleFontSource = "<link id='gt-focus-custom-text-font' href='" + googleFontPath + "' rel='stylesheet' type='text/css'>";
-			var checkLink = $( "head" ).find( "#gt-focus-custom-text-font" ).length;
-
-			if (checkLink > 0) {
-				$( "head" ).find( "#gt-focus-custom-text-font" ).remove();
-			}
-			$( "head" ).append( googleFontSource );
+			// Load Font in Customizer.
+			loadCustomFont( newval, 'text-font' );
 
 			// Set Font.
 			var systemFont = '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif';
@@ -294,16 +286,8 @@
 	wp.customize( 'gt_focus_theme_options[title_font]', function( value ) {
 		value.bind( function( newval ) {
 
-			// Embed Font.
-			var fontFamilyUrl = newval.split( " " ).join( "+" );
-			var googleFontPath = "https://fonts.googleapis.com/css?family=" + fontFamilyUrl + ":400,700";
-			var googleFontSource = "<link id='gt-focus-custom-title-font' href='" + googleFontPath + "' rel='stylesheet' type='text/css'>";
-			var checkLink = $( "head" ).find( "#gt-focus-custom-title-font" ).length;
-
-			if (checkLink > 0) {
-				$( "head" ).find( "#gt-focus-custom-title-font" ).remove();
-			}
-			$( "head" ).append( googleFontSource );
+			// Load Font in Customizer.
+			loadCustomFont( newval, 'title-font' );
 
 			// Set Font.
 			var systemFont = '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif';
@@ -334,16 +318,8 @@
 	wp.customize( 'gt_focus_theme_options[navi_font]', function( value ) {
 		value.bind( function( newval ) {
 
-			// Embed Font.
-			var fontFamilyUrl = newval.split( " " ).join( "+" );
-			var googleFontPath = "https://fonts.googleapis.com/css?family=" + fontFamilyUrl + ":400,700";
-			var googleFontSource = "<link id='gt-focus-custom-navi-font' href='" + googleFontPath + "' rel='stylesheet' type='text/css'>";
-			var checkLink = $( "head" ).find( "#gt-focus-custom-navi-font" ).length;
-
-			if (checkLink > 0) {
-				$( "head" ).find( "#gt-focus-custom-navi-font" ).remove();
-			}
-			$( "head" ).append( googleFontSource );
+			// Load Font in Customizer.
+			loadCustomFont( newval, 'navi-font' );
 
 			// Set Font.
 			var systemFont = '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif';
@@ -414,6 +390,31 @@
 
 	function isColorDark( hexColor ) {
 		return ( getColorBrightness( hexColor ) <= 130 );
+	}
+
+	function loadCustomFont( font, type ) {
+		if (typeof gtFocusFontSettings === 'undefined') {
+			return;
+		}
+
+		var defaultFonts = ['Arial', 'Arial Black', 'Courier New', 'Georgia', 'Helvetica',
+			'Impact', 'Palatino, Palatino Linotype', 'SystemFontStack', 'Tahoma',
+			'Trebuchet MS, Trebuchet', 'Times New Roman, Times', 'Verdana'];
+
+		if( defaultFonts.includes( font ) ) {
+			return;
+		}
+
+		var fontFile = font.split( " " ).join( "-" ).toLowerCase();
+		var fontFileURL = gtFocusFontSettings.pluginURL + fontFile + ".css";
+
+		var fontStylesheet = "<link id='gt-focus-custom-" + type + "' href='" + fontFileURL + "' rel='stylesheet' type='text/css'>";
+		var checkLink = $( "head" ).find( "#gt-focus-custom-" + type ).length;
+
+		if (checkLink > 0) {
+			$( "head" ).find( "#gt-focus-custom-" + type ).remove();
+		}
+		$( "head" ).append( fontStylesheet );
 	}
 
 } )( jQuery );
